6d350eb0fd7267e25bb78332c37d39b127cbfb98
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-07-02 03:01:48 +1200

    Brackeys Movement

---
+++ PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float runSpeed = 40;
+
+    private float horizontalMove;
+
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
