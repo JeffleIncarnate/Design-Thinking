2eea516395736becfa05ebbd39180f440d43cfc7
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-07-02 03:01:16 +1200

    Added Menu Script

---
+++ Menu/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject intro;
+    public GameObject prophacy;
+    public GameObject you;
+
+    private int i = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            i++;
+        }
+
+        if (i == 0)
+        {
+            intro.SetActive(true);
+        }
+        else if (i == 1)
+        {
+            intro.SetActive(false);
+            prophacy.SetActive(true);
+        }
+        else if (i == 2)
+        {
+            prophacy.SetActive(false);
+            you.SetActive(true);
+        }
+        else if (i == 3)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
